860fbd62a05eeb5d04f90f0efaf03266e88bb535
oscar <15737128231@163.com>
2016-08-06 13:16:04 +0800

    界面布局及jar包导入

---
+++ src/main/java/com/oscar/howold/Constant.java
@@ -0,0 +1,9 @@
+package com.oscar.howold;
+
+/**
+ * Created by Administrator on 2016/8/6 0006.
+ */
+public class Constant {
+    public static final String KEY = "49031b0efaad184207633c42d41aef29";
+    public static final String SECEATE = "l7o0Nc1HxiGSYOlPwTwkEBlh5diWsMSr";
+}
